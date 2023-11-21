84c3160cffaf70a962a81ff1ca413e04e0e773aa
Alan Jacobs <ahjacobs@inf.ufsm.br>
2023-11-21 14:55:30 -0300

    git add

---
+++ producao1.hs
@@ -0,0 +1,42 @@
+-- criando struct
+data Funcionario = Funcionario{
+    nomeFunc :: String,
+    salarioFunc :: Double,
+    idadeFunc :: Int
+} deriving(Show)
+
+-- calculo do beneficio
+calculoBeneficio :: Funcionario -> Double
+calculoBeneficio funcionario = salarioFunc funcionario * 0.1 --10% nesse caso
+
+-- calculo do imposto
+calculoImposto :: Funcionario -> Double
+calculoImposto funcionario = salarioFunc funcionario * 0.2 -- 20% do salario como imposto
+
+-- processando uma lista de funcionarios
+processaFunc :: [Funcionario] -> [(Double, Double)]
+processaFunc funcionarios = map(\func -> (calculoBeneficio func, calculoImposto func)) funcionarios
+
+-- le os dados dos funcionarios
+leFuncionariosDoArquivo :: FilePath -> IO [Funcionario]
+leFuncionariosDoArquivo caminhoArquivo = do
+    conteudo <- readFile caminhoArquivo
+    return $ map parseFuncionario (lines conteudo)
+
+-- cada linha do arq = 1 funcionario
+parseFuncionario :: String -> Funcionario
+parseFuncionario linha =
+    let [nome, salario, idade] = words (map (\c -> if c == ',' then ' ' else c) linha)
+    in Funcionario nome (read salario) (read idade)
+
+-- escreve o resultado no arquivo
+escreveResultado :: FilePath -> [(Double, Double)] -> IO ()
+escreveResultado caminhoArquivo resultados =
+    writeFile caminhoArquivo (unlines (map (\(beneficio, imposto) -> "Beneficio: " ++ show beneficio ++ ", Imposto: " ++ show imposto) resultados))
+
+-- main
+main :: IO ()
+main = do
+    funcionarios <- lerFuncionariosDoArquivo "funcionarios.txt"
+    let resultados = processarFuncionarios funcionarios
+    escreverResultado "resultados.txt" resultados
